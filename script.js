b1c0a6b818ef5d4ad085017c24c9647fba0a8c0c
Angye Laverde <222027155+angyelaverde@users.noreply.github.com>
2026-05-04 08:30:41 -0500

    cambio de console log a alert
    es la mejor forma de mandar mensaje al usuario

--- script.js
+++ script.js
@@ -24,19 +24,19 @@ const saludPorcentaje = 0.4;
 //condicionales
 if (edad < 18) {
   // Si el usuario es menor de edad
-  console.log("No es posible continuar con el siguiente paso ni calcular las prestaciones de ley");
+  alert("No es posible continuar con el siguiente paso ni calcular las prestaciones de ley");
 
 } else if (edad < 25) {
   // Si es mayor o igual a 18, pero menor de 25 años
-  console.log("Usuario beneficiario por cotizante. No es posible continuar al siguiente paso");
+  alert("Usuario beneficiario por cotizante. No es posible continuar al siguiente paso");
 
 } else if (edad >= 60) {
   // Si la edad es 60 años o más
-  console.log("Solo se calculará el pago de la pensión. Por favor, ingrese la información de su mesada pensional en el siguiente paso");
+  alert("Solo se calculará el pago de la pensión. Por favor, ingrese la información de su mesada pensional en el siguiente paso");
 
 } else {
   // Si no cumple ninguna de las anteriores (es decir, tiene entre 25 y 59 años)
-  console.log("Puede continuar con el siguiente paso del proceso");
+  alert("Puede continuar con el siguiente paso del proceso");
   
 }
 
